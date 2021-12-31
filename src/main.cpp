8af7e85c5e9c52abf37de8ebfd9a4468bb75cf1d
aot93 <85035551+aot93@users.noreply.github.com>
2021-12-31 12:52:09 +0000

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,27 @@
+/*
+  Copyright (c) 2021 Historic Electric Ltd
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+  The above copyright notice and this permission notice shall be included
+  in all copies or substantial portions of the Software.
+
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.cur
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
+
 #include <Metro.h>
 #include <FlexCAN_T4.h>
 #include <SwitecX12.h>
@@ -718,7 +742,7 @@ void displayinfo()
   //Power Bar..
   display_info.drawRFrame(1, 36, 84, 14, 1);     //Outline
   display_info.drawHVLine(30,17,16,1); //zero line
-  powerBar = map(kwInst, -20, 50, 0, 84);        //Map -40 to 75Kw
+  powerBar = map(kwInst, -20, 50, 0, 84);        //Map -20 to 50Kw
   display_info.drawRBox(1, 36, powerBar, 14, 1); // Bar
 }
 
